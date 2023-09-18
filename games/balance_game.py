55f660c2be38ba974cfae389a3a9c9d971ee8674
flap <savemoree@gmail.com>
2023-09-18 21:57:26 +0300

    Готовое дз №5

---
+++ games/balance_game.py
@@ -0,0 +1,47 @@
+#Функция пополнения счета
+def incr_balance(current_balance):
+    current_balance += int(input('Введите сумму для пополнения:\n'))
+    return  current_balance
+#Функция покупки
+def purchace(current_balance,current_history):
+    price_of_purch = int(input('Введите сумму для покупки:\n'))
+    if price_of_purch > current_balance:
+        print('Недостаточно средств для совершения покупки\n')
+    else:
+        name_of_purch = input('Введите название покупки:\n')
+        current_balance -= price_of_purch
+        current_history[name_of_purch] = price_of_purch
+    return current_balance, current_history
+#Функция просмотра истории
+def history_of_purchaces(current_history):
+    print('История покупок:')
+    for i, j in current_history.items():
+            print(f'{i} ---> {j}')
+    print('')
+
+def balance_game():
+    # Объявление переменных
+    balance = 0
+    history = {}
+    #Логика терминала
+    while True:
+        print('1. пополнение счета')
+        print('2. покупка')
+        print('3. история покупок')
+        print('4. выход\n')
+        #Выбираем пункт
+        choice = input('Выберите пункт:\n')
+        #Выполняем действия согласно выбранному пункту
+        match choice:
+            case '1':
+                balance = incr_balance(balance)
+                print(f'Ваш баланс:\n{balance} руб\n')
+            case '2':
+                balance, history = purchace(balance,history)
+                print(f'Ваш баланс:\n{balance} руб\n')
+            case '3':
+                history_of_purchaces(history)
+            case '4':
+                break
+            case _:
+                pass
---
+++ main_logic.py
@@ -0,0 +1,56 @@
+from MyFunc import *
+from games.victory import victory_game
+from games.balance_game import balance_game
+
+while True:
+    print('1.Создать папку')
+    print('2.Удалить папку')
+    print('3.Копировать папку')
+    print('4.Создать файл')
+    print('5.Удалить файл')
+    print('6.Копировать файл')
+    print('7.Просмотр содержимого рабочей директории')
+    print('8.Посмотреть только папки')
+    print('9.Посмотреть только файлы')
+    print('10.Просмотр информации о ОС')
+    print('11.Создатель программы')
+    print('12.Играть в викторину')
+    print('13.Мой банковйский счет')
+    print('14.Смена рабочей директории')
+    print('15.Выход')
+
+    user_choose = input('Введите номер пункта:\n')
+
+    match user_choose:
+        case '1':
+            create_folder()
+        case '2':
+            delete_folder()
+        case '3':
+            copy_folder()
+        case '4':
+            create_file()
+        case '5':
+            delete_file()
+        case '6':
+           copy_file()
+        case '7':
+            list_folder()
+        case '8':
+            list_only_folder()
+        case '9':
+            list_only_files()
+        case '10':
+            system_info()
+        case '11':
+            user_info()
+        case '12':
+            victory_game()
+        case'13':
+            balance_game()
+        case'14':
+            change_directory()
+        case'15':
+            exit()
+        case _:
+            pass
